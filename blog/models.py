e196bcf19704195fa2b0dcddf3a28e4bf081bb9b
Dmitriy Andreyuk <Dmitriy Andreyuk>
2017-01-22 01:36:25 +0300

    add photo models

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
 # Create your models here.
 
 class Post(models.Model):
@@ -9,6 +8,11 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     published_date = models.DateTimeField(blank=True, null=True)
+    photo = models.ImageField(
+        upload_to='static/images/',
+        blank=True,
+        null=True,
+    )
 
     def publish(self):
         self.published_date = timezone.now()
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render,  render_to_response, get_object_or_404
 from django.utils import timezone
 from .models import Post
-
 # Create your views here.
 
 def blog(request):
@@ -13,3 +12,4 @@ def blog(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'post_detail.html', {'post': post})
+
