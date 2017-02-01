2cb70a9e9f439351ca2cc1336084e42ebed8491c
Dmitriy Andreyuk <Dmitriy Andreyuk>
2017-02-01 10:31:53 +0200

    fixed some bugs

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+	fieldsets = (
+		('На странице блога', {
+			'fields': ('post_photo',)
+			}),
+		("Пост:", {
+			'fields': ('post_author','post_title','post_text','post_date')
+			})
+		)
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -8,12 +8,13 @@ class Post(models.Model):
     class Meta():
         db_table = 'posts'
 
-    post_author = models.ForeignKey('auth.User')
+    post_author = models.ForeignKey('auth.User', default=1)
     post_title = models.CharField(max_length=200)
     post_photo = models.ImageField(
         upload_to='blog_titels/',
         blank=True,
         null=True,
+
     )
     post_text = RichTextUploadingField()
     post_date = models.DateTimeField(blank=True, null=True)
